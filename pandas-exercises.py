04f15bc73e9411e29106f721d07bd77a5271468c
SoyStillGood <zachzemokhol@gmail.com>
2019-10-26 11:54:41 -0400

    First commit 3

--- pandas-exercises.py
+++ pandas-exercises.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os
+import os.path
 
-insurance = pd.read_csv(filepath_or_buffer='Users\zachz\Desktop\insurance.csv', sep=',', header=0)
+
+insurance = pd.read_csv(filepath_or_buffer='\Users\zachz\Desktop\insurance.csv', sep=',', header=0)
 print(insurance)
